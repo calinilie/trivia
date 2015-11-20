f70e7407591b41adec43e14172a3c96763e66030
Calin <calinilielaur@gmail.com>
2015-11-20 19:42:21 +0100

    batch file for comparin against goldenmaster 2

--- C#/Trivia/Trivia/GameRunner.cs
+++ C#/Trivia/Trivia/GameRunner.cs
@@ -4,69 +4,69 @@ using UglyTrivia;
 
 namespace Trivia
 {
-    public class GameRunner
-    {
+	public class GameRunner
+	{
+		private static bool _notAWinner;
+		private static FileStream _ostrm;
+		private static StreamWriter _writer;
 
-        private static bool _notAWinner;
-		static FileStream _ostrm;
-		static StreamWriter _writer;
-	    private const int CHANGE_ITERATION = 5;
+		public static void Main(String[] args)
+		{
+			InputParameters input = ReadInput(args);
 
-	    public static void Main(String[] args)
-	    {
-		    int seed = 5678;
+			for (int i = 0; i < input.Samples; i++)
+			{
+				int mySeed = input.Seed + i*313;
+				CreateFile(i, mySeed, input.Iteration);
 
-		    for (int i = 0; i < 10000; i++)
-		    {
-				int mySeed = seed + i * 313;
-				CreateFile(i, mySeed);
+				Game aGame = new Game(Console.WriteLine);
 
-			    Game aGame = new Game(Console.WriteLine);
+				aGame.add("Chet");
+				aGame.add("Pat");
+				aGame.add("Sue");
+				try
+				{
+					Random rand = new Random(mySeed);
 
-			    aGame.add("Chet");
-			    aGame.add("Pat");
-			    aGame.add("Sue");
-			    try
-			    {
-				    Random rand = new Random(mySeed);
+					do
+					{
+						aGame.roll(rand.Next(5) + 1);
 
-				    do
-				    {
-					    aGame.roll(rand.Next(5) + 1);
-
-					    if (rand.Next(9) == 7)
-					    {
-						    _notAWinner = aGame.wrongAnswer();
-					    }
-					    else
-					    {
-						    _notAWinner = aGame.wasCorrectlyAnswered();
-					    }
-				    } while (_notAWinner);
-			    }
-			    catch (Exception ex)
-			    {
-				    Console.WriteLine(ex);
-			    }
+						if (rand.Next(9) == 7)
+						{
+							_notAWinner = aGame.wrongAnswer();
+						}
+						else
+						{
+							_notAWinner = aGame.wasCorrectlyAnswered();
+						}
+					} while (_notAWinner);
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine(ex);
+				}
 				CloseStream();
-		    }
-	    }
+			}
+		}
 
 
-	    static void CloseStream()
-	    {
+		private static void CloseStream()
+		{
 			_writer.Close();
 			_ostrm.Close();
-	    }
+		}
 
-	    static void CreateFile(int index, int seed)
-	    {
+		private static void CreateFile(int index, int seed, int iteration)
+		{
 			try
 			{
-				var path = "../../Change"+ CHANGE_ITERATION;
+
+				var path = "../../Change" + iteration;
 				//var path = "../../GoldenMaster";
 				Directory.CreateDirectory(path);
-				_ostrm = new FileStream(string.Format("{2}/Game{0}_{1}.txt", index, seed, path), FileMode.OpenOrCreate, FileAccess.Write);
+				_ostrm = new FileStream(string.Format("{2}/Game{0}_{1}.txt", index, seed, path), FileMode.OpenOrCreate,
+					FileAccess.Write);
 				_writer = new StreamWriter(_ostrm);
 			}
 			catch (Exception e)
@@ -76,8 +76,49 @@ namespace Trivia
 				return;
 			}
 			Console.SetOut(_writer);
-	    }
-    }
+		}
 
+
+		private static InputParameters ReadInput(string[] args)
+		{
+			InputParameters input = new InputParameters
+			{
+				Iteration =  1,
+				Samples = 10000,
+				Seed = 5678
+			};
+
+			if (args == null || args.Length == 0)
+				return input;
+				
+
+			if (args.Length > 0)
+				input.Iteration = ReadInt(args[0]);
+
+			if (args.Length > 1)
+				input.Samples = ReadInt(args[1]);
+
+			if (args.Length > 2)
+				input.Seed = ReadInt(args[2]);
+
+			return input;
+		}
+
+		private static int ReadInt(string arg)
+		{
+			int retVal;
+			if (int.TryParse(arg, out retVal))
+				return retVal;
+
+			throw new ArgumentException("All parameters should be int");
+		}
+
+		private class InputParameters
+		{
+			public int Seed { get; set; }
+			public int Samples { get; set; }
+			public int Iteration { get; set; }
+		}
+	}
 }
 
